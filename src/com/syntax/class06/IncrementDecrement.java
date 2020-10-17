ae5d9006f7a5274de34b079614c866f8dedba9f2
ChristinaPROG <cwag.ramirez4@gmail.com>
2020-10-17 13:41:31 -0400

    while loop demo

---
+++ src/com/syntax/class06/IncrementDecrement.java
@@ -0,0 +1,86 @@
+package com.syntax.class06;
+
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing numbers 1 to 50-----");
+		
+		int num=0;
+while(num<51) {
+	
+	System.out.println(num);
+	num++;
+	
+}
+	
+	//I want to print 1-60
+System.out.println("Printing numbers 1 to 60-----");
+int a=10;
+while(a<61) {	
+	System.out.println(a);
+a++;
+
+}
+
+//I want to print 100-60
+System.out.println("Printing numbers 100-60-----");
+int b=100;
+while(b<6) {	
+	System.out.println(b);
+b--;
+}
+//I want to print 10 to 30 only even numbers
+System.out.println("Printing numbers 10 to 30-----");
+int c=10;
+while(c<31) {	
+if(c %2==0) {
+	
+	System.out.print(c+""); }
+
+c++;
+} 
+//
+int d=10;
+while (d<=30); {
+System.out.println(d+"");
+d+=2;
+
+	}
+	{
+
+	int z=10;
+	while(z<31) {	
+	if(z %2==0) {
+		
+		System.out.print(z+""); }
+
+///
+	
+
+{
+int e=99;
+while(e> 0); {
+
+System.out.println(e+"");
+
+
+
+}
+}
+	}
+	
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+
+
