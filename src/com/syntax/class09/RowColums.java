049957fc9d799f410b6117217935da3ad614d917
ChristinaPROG <cwag.ramirez4@gmail.com>
2020-10-20 22:02:08 -0400

    10pm

---
+++ src/com/syntax/class09/RowColums.java
@@ -0,0 +1,24 @@
+package com.syntax.class09;
+
+public class RowColums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		for (int row=7; row<=1; row--) {
+			for (int col=1; col<=6; col++) {
+				System.out.print(row);
+			}System.out.println();
+		}}
+	
+		
+	
+	
+	
+	
+	
+	}
+
+
---
+++ src/com/syntax/class09/RowsandMore.java
@@ -0,0 +1,26 @@
+package com.syntax.class09;
+
+public class RowsandMore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	///	What will be the output of the following program?
+
+			
+		boolean a, b, c;
+	     a = b = c = true;
+
+	          if( !a || ( b && c ) ) {
+	             System.out.println("If executed");
+	          } else {
+	             System.out.println("else executed");
+	          }
+
+
+	}
+
+		}
+		
+	
